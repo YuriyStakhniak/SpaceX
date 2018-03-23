717cfeca6f37926bc45f3360b75a5b869b4358a5
yurezzz13 <yurkadurka@gmail.com>
2018-03-23 15:43:12 +0200

    Mistakes in Rocket and Button class

--- src/Rocket.java
+++ src/Rocket.java
@@ -3,47 +3,35 @@ abstract class Rocket {
 	static final int ORBITAL_SPEED = 800000000;
 
 	static class Cabin {
-
 		Engine engine = new Engine();
-
 		void start(int destination, int rocketPower, int rocketWeight) {
-
 			if (fuelEnoughCheck(int rocketPower, int rocketWeight)>= destination) {
-
 				startConditionsInfo(rocketPower, rocketWeight);
-
 			} else {
-
 				startFailedConditionsInfo(rocketPower, rocketWeight);
 			}
 		}
 
 		int fuelConsumptionCalculating(int rocketPower, int rocketWeight) {
-
 			return rocketPower * rocketWeight;
-
 		}
 
 		void startConditionsInfo(int rocketPower, int rocketWeight) {
-
 			System.out.println("You've got " + engine.getFuel() + " gallons of fuel");
 			System.out.println("Value of fuel consumption " + fuelConsumptionCalculating(rocketPower, rocketWeight)
 					+ " gallons per mile");
 			System.out.println("We are going to Mars");
-
 		}
 
 		void startFailedConditionsInfo(int rocketPower, int rocketWeight) {
-
 			System.out.println("Sorry, you stay at home");
 			System.out.println("You've got " + engine.getFuel() + " gallons of fuel");
 			System.out.println("You should know " + "value of fuel consumption in this case might be "
 					+ fuelConsumptionCalculating(rocketPower, rocketWeight) + " gallons per mile");
-
 		}
 
 		int fuelEnoughCheck(int rocketPower, int rocketWeight){
-			return engine.getFuel() / fuelConsumptionCalculating(rocketPower, rocketWeight)
+			return engine.getFuel() / fuelConsumptionCalculating(rocketPower, rocketWeight);
 		}
 	}
 }
--- src/StartButton.java
+++ src/StartButton.java
@@ -1,19 +1,13 @@
-
 public class StartButton {
 
 	public static void pushStartButton(int rocketPower, int rocketWeight, String planetName) {
-
 		Rocket.Cabin cabin = new Rocket.Cabin();
 		SpaceX spaceX = new SpaceX();
-
 		if (planetName.equals(Planets.MARS.name)) {
 			cabin.start(rocketPower, rocketWeight, Planets.MARS.destination);
 			spaceX.getTimeToOrbitalSpeed(rocketPower, rocketWeight);
-
 		} else {
 			System.out.println("Sorry we can't get here");
 		}
-
 	}
-
 }
