408d060b8c9c2332c2522a526c81648935070bb7
yurezzz13 <yurkadurka@gmail.com>
2018-03-23 10:35:58 +0200

    getTimeToOrbitalSpeed method fixed

--- src/SpaceX.java
+++ src/SpaceX.java
@@ -1,17 +1,9 @@
 public class SpaceX extends Rocket implements RocketParamCalculating {
 
-    @Override
+	@Override
 
-    public void getTimeToOrbitalSpeed(int rocketPower, int rocketWeight) {
+	public void getTimeToOrbitalSpeed(int rocketPower, int rocketWeight) {
 
-        int timeToOrbitalSpeed;
-
-        timeToOrbitalSpeed = Rocket.ORBITAL_SPEED / (rocketWeight + rocketPower);
-
-        System.out.println("Time to orbital speed " + timeToOrbitalSpeed);
-    }
+		System.out.println("Time to orbital speed " + Rocket.ORBITAL_SPEED / (rocketWeight + rocketPower));
+	}
 }
-
-
-
-
