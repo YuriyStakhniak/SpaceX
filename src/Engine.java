4758e44714a0099465788f851e0c8e687ab4878f
yurezzz13 <yurkadurka@gmail.com>
2018-03-23 09:31:51 +0200

    engine class modification

--- src/Engine.java
+++ src/Engine.java
@@ -4,10 +4,6 @@ class Engine {
 
         long minTankCapacity = 1000000000000000L;
         long maxTankCapacity = 2000000000000000L;
-        long fuel;
-
-        fuel = minTankCapacity + (long) (Math.random() * maxTankCapacity);
-
-        return fuel;
+        return fuel = minTankCapacity + (long) (Math.random() * maxTankCapacity);
     }
 }
